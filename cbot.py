93076cdada7ca3983c3c04bcb4faa08432106324
MaZurd01 <42522353+MaZurd01@users.noreply.github.com>
2018-12-28 23:50:52 +0200

    Update cbot.py

--- cbot.py
+++ cbot.py
@@ -1,27 +1,49 @@
 import discord
-import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+import random
+import os
 
-client = discord.Client()
+bot = discord.Client()
+bot_prefix="!"
+bot = commands.Bot(command_prefix=bot_prefix)
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+@bot.event
+async def on_ready() :
+    print("Bot çevrimiçi!")
+    print("İsim : {}".format(bot.user.name))
+    print("ID : {}".format(bot.user.id))
+    print(str(len(bot.servers)) + " tane serverda çalışıyor!")
+    print(str(len(set(bot.get_all_members()))) + " tane kullanıcaya erişiyor!")
+    await bot.change_presence(game=discord.Game(name='2318 botu çalışır durumda!'))
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+@bot.command(pass_context=True)
+async def yirmi15(ctx) :
+    await bot.say("Ben bot 2318'im!")
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+@bot.command(pass_context=True)
+async def a(ctx, member:discord.Member):
+    urll = ["https://media1.tenor.com/images/49a21e182fcdfb3e96cc9d9421f8ee3f/tenor.gif" , 
+            "https://media1.tenor.com/images/b0de026a12e20137a654b5e2e65e2aed/tenor.gif" ,
+           "https://78.media.tumblr.com/680b69563aceba3df48b4483d007bce3/tumblr_mxre7hEX4h1sc1kfto1_500.gif"]
+    embed = discord.Embed(title=ctx.message.author.name + " sana sarılıyor " + member.name )
+    embed.set_image(url=random.choice(urll))
+    await bot.say(embed=embed)
 
-client.run(process.env.TOKEN4)
+@bot.command(pass_context=True)
+async def sil (ctx, number):
+    mgs = []
+    number = int(number)
+    async for x in bot.logs_from(ctx.message.channel, limit=number):
+        mgs.append(x)
+    await bot.delete_messages(mgs)
+
+@bot.command(pass_context=True)
+async def s (ctx):
+    mgs = ctx.message.content.split("1")
+    await bot.delete_message(ctx.message)
+    await bot.send_message(ctx.message.channel , mgs)
+
+
+
+bot.run(os.environ.get('TOKEN4'))
